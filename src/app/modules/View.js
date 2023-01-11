d90bb7f3b825efb029747ad9a38cd43af2566a1b
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-01-11 15:57:08 -0500

    add styling

---
+++ src/app/modules/View.js
@@ -0,0 +1,14 @@
+class View {
+  constructor() {
+    this.elements = {
+      p1Board: document.getElementById('p1-board'),
+      p2Board: document.getElementById('p2-board'),
+    };
+  }
+
+	renderBoard(grid) {
+
+	}
+}
+
+module.exports = View;
