ae193abf0362f3077846bbe91528734abaaf47e8
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-01-11 13:17:11 -0500

    set up css loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,4 +16,16 @@ module.exports = {
 			}
 		),
 	],
+	module: {
+		rules: [
+			{
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+			{
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+		],
+	}
 };
